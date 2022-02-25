17867ba33622815894b0e1b4d64ce5d1e55592c4
William Hobson <will.hobson85@gmail.com>
2022-02-24 22:32:35 -0500

    Update variables

--- variables.js
+++ variables.js
@@ -2,3 +2,13 @@ Strings:
 var catName = "Stone";
 var comicName = "Spider-Man";
 var drink = "Sprite";
+
+Numbers:
+var catAge = 8
+var issueNumber = 129
+var drinkSize = 24
+
+Booleans:
+var catOld = true
+var issueExpensive = true
+var drinkBig = true
